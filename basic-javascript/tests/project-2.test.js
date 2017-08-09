9e843a40d961e7e8ef0cfb27d8020180e21434b7
WizrdJim <wzrdjim@gmail.com>
2017-08-09 17:01:50 -0400

    busy work

--- basic-javascript/tests/project-2.test.js
+++ basic-javascript/tests/project-2.test.js
@@ -2,3 +2,204 @@ const assert = require('chai').assert;
 const funcs = require('../src/project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-2 functions', () => {
+  describe('getBiggest', () => {
+    it('Should be a function', ()=>{
+      const getBiggest = funcs.getBiggest;
+      assert.typeOf(getBiggest, 'function');
+    })
+    it('Should return the bigger value', ()=> {
+      const getBiggest = funcs.getBiggest;
+      assert.equal(getBiggest(1,2), 2);
+    })
+  })
+  describe('greeting', () => {
+    it('Should be a function', ()=>{
+      const greeting = funcs.greeting;
+      assert.typeOf(greeting, 'function');
+    })
+    it('Should call the corresponding greeting', () => {  
+      const germ = funcs.greeting('German');
+      const span = funcs.greeting('Spanish');
+      assert.equal(germ, 'Guten Tag!');
+      assert.equal(span, 'Hola!');
+    })
+  })
+  describe('isTenOrFive', () => {
+    it('Should be a function', ()=>{
+      const isTenOrFive = funcs.isTenOrFive;
+      assert.typeOf(isTenOrFive, 'function');
+    })
+    it('Should return if its a 10 or 5', ()=> {
+      const isTenOrFive = funcs.isTenOrFive;
+      assert.isTrue(isTenOrFive(10));
+      assert.isTrue(isTenOrFive(5));
+    })
+  })
+  describe('isInRange', () => {
+    it('Should be a function', () => {
+      const isInRange = funcs.isInRange;
+      assert.isFunction(isInRange);
+    })
+    it('Should check if less than 50 and greater than 20', () => {
+      const isInRange = funcs.isInRange;
+      assert.isTrue(isInRange(30));
+    })
+  })
+  describe('isInteger', () => {
+    it('Should be a function', () => {
+      const isInteger = funcs.isInteger;
+      assert.isFunction(isInteger);
+    })
+    it('Should tell if the number is an integer', () => {
+      const isInteger = funcs.isInteger;
+      assert.isTrue(isInteger(2));
+    })
+  })
+  describe('fizzBuzz', () => {
+    it('Should be a function', () => {
+      const fizzBuzz = funcs.fizzBuzz;
+      assert.isFunction(fizzBuzz);
+    })
+    it('Should return words that may or may not mean anything', () => {
+      const fizzBuzz = funcs.fizzBuzz;
+      assert.equal(fizzBuzz(15), 'fizzbuzz');
+      assert.equal(fizzBuzz(10), 'buzz');
+      assert.equal(fizzBuzz(9), 'fizz');
+    })
+  })
+  describe('isPrime', () => {
+    it('Should be a function', ()=>{
+      const isPrime = funcs.isPrime;
+      assert.typeOf(isPrime, 'function');
+    })
+    it('Should tell if the number is a prime Number', () =>{
+      const isPrime = funcs.isPrime;
+      assert.isOk(isPrime(3));
+      assert.isNotOk(isPrime(0));
+    })
+  })
+  describe('returnFirst', () => {
+    it('Should be a function', ()=>{
+      const returnFirst = funcs.returnFirst;
+      assert.typeOf(returnFirst, 'function');
+    })
+    it('Should return the first number in the array', () =>{
+      const returnFirst = funcs.returnFirst;
+      const arr = [3,4,0];
+      assert.equal(returnFirst(arr), 3);    
+    })
+  })
+  describe('returnLast', () => {
+    it('Should be a function', ()=>{
+      const returnLast = funcs.returnLast;
+      assert.typeOf(returnLast, 'function');
+    })
+    it('Should return the last item in the array', () =>{
+      const returnLast = funcs.returnLast;
+      const arr = [3,4,0];
+      assert.equal(returnLast(arr), 0);
+    });
+  })
+  describe('getArrayLength', () => {
+    it('Should be a function', ()=>{
+      const getArrayLength = funcs.getArrayLength;
+      assert.typeOf(getArrayLength, 'function');
+    })
+    it('Should return the length of given array', () =>{
+      const getArrayLength = funcs.getArrayLength;
+      const arr = [3, 0, 4];
+      assert.equal(getArrayLength(arr), 3);
+    })
+  })
+  describe('incrementByOne', () => {
+    it('Should be a function', ()=>{
+      const incrementByOne = funcs.incrementByOne;
+      assert.typeOf(incrementByOne, 'function');
+    })
+    it('Should increment an arrays values by 1', ()=>{
+      const incrementByOne = funcs.incrementByOne;
+      const arr = [3, 0, 4];
+      const result = incrementByOne(arr);
+      assert.deepEqual(result, [4, 1, 5]);
+    })
+  })
+  describe('addItemToArray', () => {
+    it('Should be a function', ()=> {
+      const addItemToArray = funcs.addItemToArray;
+      assert.typeOf(addItemToArray, 'function');
+    })
+    it('Should add items to array', () =>{
+      const addItemToArray = funcs.addItemToArray;
+      const arr = [3, 0, 4];
+      assert.deepEqual(addItemToArray(arr, 4),[3, 0, 4, 4] )
+    })
+  })
+  describe('addItemToFront', () => {
+    it('Should be a function', ()=>{
+      const addItemToFront = funcs.addItemToFront;
+      assert.typeOf(addItemToFront, 'function');
+    })
+    it('Should add the item to the front of the array', () => {
+      const addItemToFront = funcs.addItemToFront;
+      const arr = [3, 0, 4];
+      assert.deepEqual(addItemToFront(arr, 5), [5, 3, 0, 4]);
+    })
+  })
+  describe('wordsToSentence', () => {
+    it('Should be a function', ()=>{
+      const wordsToSentence = funcs.wordsToSentence;
+      assert.typeOf(wordsToSentence, 'function');
+    })
+    it('should turn an array of words into a sentence', () => {
+      const wordsToSentence = funcs.wordsToSentence;
+      const arr = ['Hello', 'World'];
+      assert.equal(wordsToSentence(arr), 'Hello World');
+    })
+  })
+  describe('contains', () => {
+    it('Should be a function', ()=>{
+      const contains = funcs.contains;
+      assert.typeOf(contains, 'function');
+    })
+    it('Should contain the item in the array', () => {
+      const contains = funcs.contains;
+      const arr = ['I', 'am', 'Ubermensch'];
+      assert.isTrue(contains(arr, 'Ubermensch'));
+    })
+  })
+  describe('addNumbers', () => {
+    it('Should be a function', ()=>{
+      const addNumbers = funcs.addNumbers;
+      assert.typeOf(addNumbers, 'function');
+    })
+    it('Should add an array of numbers', () => {
+      const addNumbers = funcs.addNumbers;
+      const arr = [3, 0, 4];
+      assert.equal(addNumbers(arr), 7);
+    })
+  })
+  describe('averageTestScore', () => {
+    it('Should be a function', ()=>{
+      const averageTestScore = funcs.averageTestScore;
+      assert.typeOf(averageTestScore, 'function');
+    })
+    it('Should get the avverage of an array of numbers', ()=> {
+      const averageTestScore = funcs.averageTestScore;
+      const arr = [3, 0, 4, 3];
+      assert.equal(averageTestScore(arr), 2.5);
+    })
+  })
+  describe('largestNumber', () => {
+    it('Should be a function', ()=>{
+      const largestNumber = funcs.largestNumber;
+      assert.typeOf(largestNumber, 'function');
+    })
+    it('Should return the largest number in an array', () => {
+       const largestNumber= funcs.largestNumber;
+       const arr = [3, 0, 4];
+       assert.equal(largestNumber(arr), 4);
+    })
+  })
+});
+
--- basic-javascript/tests/project-4.test.js
+++ basic-javascript/tests/project-4.test.js
@@ -1,4 +1,118 @@
+const sinon = require('sinon');
 const assert = require('chai').assert;
 const funcs = require('../src/project-4.js');
 
+
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+describe('Project-4 functions', () => {
+  describe('multiplyArguments', () => {
+    it('Should be a function', () => {
+      const multiplyArguments = funcs.multiplyArguments;
+      assert.isFunction(multiplyArguments);
+    })
+    it('should multiply arguments passed to the function', () => {
+      const multiplyArguments = funcs.multiplyArguments;
+      assert.equal(multiplyArguments(3, 4, 6), 72);
+    })
+  })
+  describe('invokeCallback', () => {
+    it('Should be a function', () => {
+      const invokeCallback = funcs.invokeCallback;
+      assert.isFunction(invokeCallback);
+    });
+    it('should invoke a callback function', () => {
+      const invokeCallback = funcs.invokeCallback;
+      const spy = sinon.spy();
+      invokeCallback(spy);
+      sinon.assert.calledOnce(spy);
+    })
+  })
+  describe('sumArray', () => {
+    it('Should be a function', () => {
+      const sumArray = funcs.sumArray;
+      assert.isFunction(sumArray);
+    })
+    it('Should pass a summed array to callback', () =>{
+      const sumArray = funcs.sumArray;
+      const spy = sinon.spy();
+      const arr = [3, 0, 4];
+      sumArray(arr, spy);
+      sinon.assert.calledWith(spy, 7);
+    })
+  })
+  describe('forEach', () => {
+    it('Should be a function', () => {
+      const forEach = funcs.forEach;
+      assert.isFunction(forEach);
+    })
+    it('Should call a function for each value in an array', () => {
+      const forEach = funcs.forEach;
+      const spy = sinon.spy();
+      const arr = [3, 0, 4];
+      forEach(arr, spy);
+      sinon.assert.calledThrice(spy);
+      sinon.assert.calledWith(spy, 3);
+      sinon.assert.calledWith(spy, 0);
+      sinon.assert.calledWith(spy, 4);
+    })
+  })
+  describe('map', () => {
+    it('Should be a function', () => {
+      const map = funcs.map;
+      assert.isFunction(map);
+    })
+    it('should modify an array with cb', () => {
+      const map = funcs.map;
+      const arr = [3, 0, 4];
+      const spy = sinon.spy();
+      const result = map(arr, spy);
+      assert.isArray(result);
+      sinon.assert.calledThrice(spy);
+    })
+  })
+  describe('getUserConstructor', () => {
+    it('Should be a function', () => {
+      const getUserConstructor = funcs.getUserConstructor;
+      assert.isFunction(getUserConstructor);
+    })
+    it('Should construct a user', () => {
+      const getUserConstructor = funcs.getUserConstructor;
+      const User = getUserConstructor();
+      
+    })
+  })
+  describe('addPrototypeMethod', () => {
+    it('Should be a function', () => {
+      const addPrototypeMethod = funcs.addPrototypeMethod;
+      assert.isFunction(addPrototypeMethod);
+    })
+  })
+  describe('addReverseString', () => {
+    it('Should be a function', () => {
+      const addReverseString = funcs.addReverseString
+      assert.isFunction(addReverseString);
+    })
+    it('Should add the prototype reverse', () => {
+      const addReverseString = funcs.addReverseString;
+      const str = 'pow';
+      addReverseString();
+      assert.equal(str.reverse(), 'wop')
+    })
+  })
+  describe('nFactorial', () => {
+    it('Should be a function', () => {
+      const nFactorial = funcs.nFactorial;
+      assert.isFunction(nFactorial);
+    })
+    it('Should produce the factorial of a number', () => {
+      const nFactorial = funcs.nFactorial;
+      assert.equal(nFactorial(3), 6);
+    })
+  })
+  describe('cacheFunction', () => {
+    it('Should be a function', () => {
+      const cacheFunction = funcs.cacheFunction;
+      assert.isFunction(cacheFunction); 
+    })
+  })
+})
